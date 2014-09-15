de554ac6e53a9c972b2fc31a7ca8ec48f63cbd00
wangcheny <383817783@qq.com>
2014-09-15 21:10:14 +0800

    Create menu.c

---
+++ menu.c
@@ -0,0 +1,112 @@
+/**************************************************************************************************/
+/* Copyright (C) mc2lab.com, SSE@USTC, 2014-2015                                                  */
+/*                                                                                                */
+/*  FILE NAME             :  menu.c                                                               */
+/*  PRINCIPAL AUTHOR      :  Wangchenyang                                                         */
+/*  SUBSYSTEM NAME        :  menu                                                                 */
+/*  MODULE NAME           :  menu                                                                 */
+/*  LANGUAGE              :  C                                                                    */
+/*  TARGET ENVIRONMENT    :  ANY                                                                  */
+/*  DATE OF FIRST RELEASE :  2014/09/14                                                           */
+/*  DESCRIPTION           :  This is a menu program                                               */
+/**************************************************************************************************/
+
+/*
+ * Revision log;
+ *
+ * Created by Wangchenyang,2014/09/14
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "linktable.h"
+#include <string.h>
+
+int Help();
+
+#define CMD_MAX_LEN 128
+#define DESC_LEN    1024
+#define CMD_NUM     10
+
+/* data struts and its operations */
+
+typedef struct DateNode
+{
+    tLinkTableNode * pNext;
+    char*   cmd;
+    char*   desc;
+    int     (*handler)();        
+} tDataNode;
+
+/* find a cmd in the linklist and return the datanode pointer */
+tDataNode* FindCmd(tLinkTable * head, char * cmd)
+{
+    tDataNode * pNode = (tDataNode*)GetLinkTableHead(head);
+    while(pNode != NULL)
+    {
+        if(!strcmp(pNode->cmd, cmd))
+        {
+            return  pNode;  
+        }
+        pNode = (tDataNode*)GetNextLinkTableNode(head,(tLinkTableNode *)pNode);
+    }
+    return NULL;
+}
+
+/* show all cmd in listlist */
+int ShowAllCmd(tLinkTable * head)
+{
+    tDataNode * pNode = (tDataNode*)GetLinkTableHead(head);
+    while(pNode != NULL)
+    {
+        printf("%s - %s\n", pNode->cmd, pNode->desc);
+        pNode = (tDataNode*)GetNextLinkTableNode(head,(tLinkTableNode *)pNode);
+    }
+    return 0;
+}
+
+/* menu program */
+
+static tDataNode data[] = 
+{
+    {NULL, "help", "this is help cmd!", Help},
+    {NULL, "version", "menu program v1.0", NULL}
+};
+
+tLinkTable * head = NULL;
+
+int main()
+{
+    head = CreateLinkTable();
+    AddLinkTableNode(head,(tLinkTableNode *)&data[0]);
+    AddLinkTableNode(head,(tLinkTableNode *)&data[1]);
+    
+   /* cmd line begins */
+    while(1)
+    {
+        char cmd[CMD_MAX_LEN];
+        printf("Input a cmd number > ");
+        scanf("%s", cmd);
+        tDataNode *p = FindCmd(head, cmd);
+        if( p == NULL)
+        {
+            printf("This is a wrong cmd!\n ");
+            continue;
+        }
+        printf("%s - %s\n", p->cmd, p->desc); 
+        if(p->handler != NULL) 
+        { 
+            p->handler();
+        }
+   
+    }
+}
+
+int Help()
+{
+    ShowAllCmd(head);
+    return 0; 
+}
+
